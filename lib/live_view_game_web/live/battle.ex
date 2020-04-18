7987501c431a6f8474556d7123dc618cd8ba13b3
Robin Clowers <robin.clowers@gmail.com>
2020-04-18 13:46:32 -0700

    Battle view handles invalid params

--- lib/live_view_game_web/live/battle.ex
+++ lib/live_view_game_web/live/battle.ex
@@ -1,8 +1,10 @@
 defmodule LiveGameWeb.Battle do
   use Phoenix.LiveView
   alias LiveGameWeb.Endpoint
+  alias LiveGameWeb.Home
   alias LiveGame.Presence
   alias LiveGame.Game
+  alias LiveGameWeb.Router.Helpers, as: Routes
   require Logger
 
   def render(assigns) do
@@ -12,14 +14,20 @@ defmodule LiveGameWeb.Battle do
   def mount(%{"attacker_id" => attacker_id, "defender_id" => defender_id}, session, socket) do
     {:ok, state} = LiveGame.Game.get_state()
     Logger.info("Loading game state: #{inspect(state)}")
+    attacker = state.players[attacker_id]
+    defender = state.players[defender_id]
 
-    {:ok,
-     assign(socket, %{
-       state: state,
-       player: state.players[session["id"]],
-       user_id: session["id"],
-       attacker: state.players[attacker_id],
-       defender: state.players[defender_id]
-     })}
+    if !attacker || !defender do
+      {:ok, push_redirect(socket, to: Routes.live_path(socket, Home, %{}))}
+    else
+      {:ok,
+       assign(socket, %{
+         state: state,
+         player: state.players[session["id"]],
+         user_id: session["id"],
+         attacker: state.players[attacker_id],
+         defender: state.players[defender_id]
+       })}
+    end
   end
 end
